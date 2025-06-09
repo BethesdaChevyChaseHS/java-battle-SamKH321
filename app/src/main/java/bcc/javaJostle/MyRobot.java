af74aaefd03733a4f7d391729a816723cd153ab4
SamKH321 <samkayoldale@gmail.com>
2025-06-09 23:57:54 +0000

    submitting

--- app/src/main/java/bcc/javaJostle/MyRobot.java
+++ app/src/main/java/bcc/javaJostle/MyRobot.java
@@ -2,11 +2,14 @@ package bcc.javaJostle;
 
 import java.util.ArrayList;
 public class MyRobot extends Robot{
+    private int curXMovement = 0;
+    private int curYMovement = 0;
     public MyRobot(int x, int y){
-        super(x, y, 3, 3, 2, 2,"bob", "myRobotImage.png", "defaultProjectileImage.png");
+        super(x, y, 3, 1, 3, 3,"The Frog", "myRobotImage.png", "defaultProjectileImage.png");
         // Health: 3, Speed: 3, Attack Speed: 2, Projectile Strength: 2
         // Total = 10
-        // Image name is "myRobotImage.png"
+        //I say for stat spread; health 1, speed 3, attack speed 3, projectle strength 3
+       
     }
 
     public void think(ArrayList<Robot> robots, ArrayList<Projectile> projectiles, Map map, ArrayList<PowerUp> powerups) {
@@ -22,6 +25,61 @@ public class MyRobot extends Robot{
          to shoot, use shootAtLocation(x, y) where x and y are the coordinates of the target
          only shoot when canAttack() is true!
         */
-       // System.out.println("Thinking...");
+       if (robots.size() < 3) {
+        
+                if(Math.random() < 0.4) {
+                    if(Math.random() < 0.1) {
+                    double r = Math.random();
+                        if (r < 0.40) {
+                            for(Robot robot : robots) {
+                                if (robot != this && robot.isAlive() ){
+                                    if(((robot.getX() > this.getX()) && (robot.getX()-this.getX() > 40)) && (this.getX() != 0)){
+                                    curXMovement = 1;
+                                    curYMovement = 0; 
+                                    }else{
+                                    curXMovement = -1;
+                                    curYMovement = 0; 
+                                    }
+                                }
+                            }
+                        } else if (r < 0.8) {
+                            for(Robot robot : robots) {
+                                if (robot != this && robot.isAlive() ){
+                                    if((robot.getY() > this.getY()) && (robot.getY()-this.getY() > 40)  && (this.getY() != 0)){
+                                    curXMovement = 0;
+                                    curYMovement = 1; 
+                                    }else{
+                                    curXMovement = 0;
+                                    curYMovement = -1; 
+                                    }
+                                }
+                            }
+                        } else if (r < 0.85) {
+                            curXMovement = 1;
+                            curYMovement = 0; 
+                        } else if (r < 0.9) {
+                            curXMovement = 1;
+                            curYMovement = 0; 
+                        } else if (r < 0.95) {
+                            curYMovement = -1; 
+                            curXMovement = 0; 
+                        } else {
+                            curYMovement = 1; 
+                            curXMovement = 0;
+                        }
+                    } 
+            }
+        }
+        xMovement = curXMovement;
+        yMovement = curYMovement;
+        if(canAttack()){
+            for(Robot robot : robots) {
+                if (robot != this && robot.isAlive() ){
+                    shootAtLocation(robot.getX() + Utilities.ROBOT_SIZE/2, robot.getY() + Utilities.ROBOT_SIZE/2);
+                    break; // Shoot at the first target found
+                }
+            }
+        }
     }
-}
+    }
+
